54285f613c0d565085a9dc3e22c26322ffde6b44
legacytrx <legacytrx@atheios.org>
2022-06-12 12:49:37 +0200

    Fixed error with uncle reward

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var logger = require('morgan');
+const logger = require("../logger");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,33 +25,43 @@ router.get('/circulatingsupply', async function(req, res, next) {
 });
 
 async function getTotalSupply() {
-  let coins=0;
+  let othercoins=0;
+  let miningcoins=0;
   let i=0;
   
-  let monitary_block= [1000000, 1000000, 700000,300000,800000,200000,1000000,1000000,1000000, 1000000,150000, 600000, 250000, 1000000, 100000];
-  let monitary_reward= [    13,      11,    9.4,   9.4,   8.1,   5.8,    4.7,   3.45,   2.45,     1.9,    11,    1.3,    1.3,    1.05,      1];
-  let monitary_special= [    0,       0,      0,     0,     0,     0,      0,      0,      0,       0,     0,22000000,      0,       0,      0];
+  let monitary_block= [1000000, 1000000, 700000,300000,800000,200000,1000000,1000000,1000000, 1000000,150000, 600000, 250000, 1000000, 1000000, 1000000, 1000000];
+  let monitary_mining= [    10,       8,    6.4,   5.6,   4.5,   2.4,    1.8,   1.25,    0.8,     0.6,    10,    0.3,    0.3,     0.3,     0.3,     0.3,     0.3];
+  let monitary_node= [       2,       2,      2,   2.8,   2.6,   2.4,    1.9,    1.4,      1,     0.8,  0.65,   0.65,   0.65,     0.5,     0.5,    0.55,    0.55];
+  let monitary_dev= [        1,       1,      1,     1,     1,     1,      1,    0.8,   0.65,     0.5, 0.35,   0.35,   0.35,    0.25,     0.2,    0.15,    0.15];
+  let monitary_special= [    0,       0,      0,     0,     0,     0,      0,      0,      0,       0,     0,21563938,     0,       0,       0,        0,      0];
   let blockheight;
   
   await web3.eth.getBlockNumber()
     .then((result)=> {
       blockheight=result;
-      for (i=0; i<monitary_block.length; i++) {
+      // We run all but the last array element
+      for (i=0; i<monitary_block.length-1; i++) {
         if (result-monitary_block[i]<0)
           break;
         else {
           result-=monitary_block[i];
-          coins+=monitary_reward[i]*monitary_block[i]+monitary_special[i];
+          // we need to distinguish minigcoins which get uncle reward and node / dev reward which is not uncle impacted
+          miningcoins+=monitary_mining[i]*monitary_block[i];
+          othercoins+=monitary_special[i]+(monitary_node[i]+monitary_dev[i])*monitary_block[i];
         }
       }
-      coins+=result*monitary_reward[i]+monitary_special[i];
-      // Uncle is approx 6.25%
-      coins=parseInt(coins*1.0625);
+      // last arrary element is run here
+      miningcoins+=monitary_mining[i]*result;
+      // Uncle is approx 3%
+      miningcoins=parseInt(miningcoins*1.03);
+      othercoins+=monitary_special[i]+(monitary_node[i]+monitary_dev[i])*result;
+      // logger.info("Last remaining blocks %s, Minigcoins tot %s, Coins tot %s", result, miningcoins, coins);
+  
     })
     .catch((error)=>{
       logger.error("#app.getNetworkStats: Error %s", error);
     })
-  return (coins);
+  return (miningcoins+othercoins);
 }
 
 async function getCirculatingSupply() {
